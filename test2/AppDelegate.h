d33e98dc4c4439e0cff07e4d297f1d65d085ee03
guolj <applekwork@163.com>
2019-01-10 21:37:09 +0800

    df

---
+++ test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test2
+//
+//  Created by guomini on 2019/1/10.
+//  Copyright © 2019 guomini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test2Tests/test2Tests.m
@@ -0,0 +1,37 @@
+//
+//  test2Tests.m
+//  test2Tests
+//
+//  Created by guomini on 2019/1/10.
+//  Copyright © 2019 guomini. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface test2Tests : XCTestCase
+
+@end
+
+@implementation test2Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
